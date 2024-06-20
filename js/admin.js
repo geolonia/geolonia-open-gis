6bc4265f44199b82580747d96fb1e6c7a56d355d
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2024-06-20 11:53:19 +0900

    autosave や undo/redo の挙動の改善

--- js/admin.js
+++ js/admin.js
@@ -75,8 +75,28 @@ if (document.getElementById('geolonia-gis-editor-container')) {
       }
     })
 
-    map.addControl(draw, 'top-right')
+    map.addControl(draw, 'top-right') // draw の各種コントロールを追加
 
+    // デフォルトの GeoJSON をセット
+    const content = document.getElementById('content').value
+
+    try {
+      const geojson = JSON.parse(content)
+      draw.set(geojson)
+      current.undo.push(geojson)
+      setFeatureCount(geojson)
+    } catch (e) {
+      // textarea が空の場合は、新規に空の GeoJSON をセット
+      const geojson = {
+        "type": "FeatureCollection",
+        "features": []
+      }
+      draw.set(geojson)
+      current.undo.push(geojson)
+      setFeatureCount(geojson)
+    }
+
+    // 地物を新しく追加した際の処理
     map.on('draw.create', (e) => {
       const featureId = e.features[0].id
 
@@ -90,39 +110,39 @@ if (document.getElementById('geolonia-gis-editor-container')) {
       setGeoJSON()
     });
 
-    map.on('draw.delete', setGeoJSON);
-    map.on('draw.update', setGeoJSON);
+    map.on('draw.delete', setGeoJSON); // 地物を削除した際の処理
+    map.on('draw.update', setGeoJSON); // 地物を更新した際の処理
 
-    const content = document.getElementById('content').value
-    if (content) {
-      const geojson = JSON.parse(content)
-      draw.set(geojson)
-      setFeatureCount(geojson)
-    }
-
-    document.getElementsByClassName('restore-backup')[0].addEventListener('click', () => {
+    // WPのオートセーブによるバックアップから復元するための処理
+    document.getElementsByClassName('restore-backup')[0].addEventListener('click', (e) => {
       const savedPostdata = window.wp.autosave.local.getSavedPostData()
       if (savedPostdata && 'maps' === savedPostdata.post_type) {
+        document.getElementById('title').value = savedPostdata.post_title
         draw.add(JSON.parse(savedPostdata.content))
+
+        setGeoJSON()
 
         document.getElementById('title').focus() // WP に全選択されるので解除
         draw.changeMode('draw_point') // スタイルを反映させるために一度モードを変更
         draw.changeMode('simple_select') // さらに元に戻す
 
-        setGeoJSON()
+        e.preventDefault()
       }
-    })
+    }, false)
 
+    // 現在の中心座標を取得してWPのメタボックスにセット
     document.getElementById('geolonia-get-latlng-button').addEventListener('click', () => {
       const lnglat = map.getCenter()
       document.getElementById('geolonia-gis-lat').value = lnglat.lat
       document.getElementById('geolonia-gis-lng').value = lnglat.lng
     })
 
+    // 現在のズームレベルを取得してWPのメタボックスにセット
     document.getElementById('geolonia-get-zoom-button').addEventListener('click', () => {
       document.getElementById('geolonia-gis-zoom').value = map.getZoom()
     })
 
+    // カラーピッカーで色を変更した際の処理
     colorPicker.on( 'change', ( picker, color ) => {
       const colorArray =  AColorPicker.parseColor(color)
       colorArray.push(0.2)
@@ -135,8 +155,10 @@ if (document.getElementById('geolonia-gis-editor-container')) {
       }
 
       draw.set(draw.getAll())
+      setGeoJSON()
     })
 
+    // 地物に対する選択/選択解除の際の処理
     map.on('draw.selectionchange', (e) => {
       if (e.features.length && e.features[0].id) {
         current.features = e.features
@@ -151,11 +173,13 @@ if (document.getElementById('geolonia-gis-editor-container')) {
       setGeoJSON()
     })
 
+    // 地物が削除された際に地図のポップアップを非表示にする
     map.on('draw.delete', () => {
       current.features = []
       toggleMetabox(false)
     })
 
+    // 各地物のタイトルを GeoJson にセット
     document.getElementById('geojson-meta-title').addEventListener('change', (e) => {
       const title = e.target.value
 
@@ -165,8 +189,10 @@ if (document.getElementById('geolonia-gis-editor-container')) {
       }
 
       draw.set(draw.getAll())
+      setGeoJSON()
     })
 
+    // Undo / Redo
     document.getElementById('geolonia-gis-editor-container').addEventListener('keydown', (e) => {
       if ('z' === e.key && false === e.shiftKey && (true === e.metaKey || true === e.ctrlKey)) {
         if (current.undo.length > 1) {
@@ -178,6 +204,8 @@ if (document.getElementById('geolonia-gis-editor-container')) {
 
             draw.changeMode('draw_point') // スタイルを反映させるために一度モードを変更
             draw.changeMode('simple_select') // さらに元に戻す
+            toggleMetabox(false)
+            current.features = []
 
             setGeoJSON()
           }
@@ -192,6 +220,8 @@ if (document.getElementById('geolonia-gis-editor-container')) {
 
             draw.changeMode('draw_point') // スタイルを反映させるために一度モードを変更
             draw.changeMode('simple_select') // さらに元に戻す
+            toggleMetabox(false)
+            current.features = []
 
             setGeoJSON()
           }
