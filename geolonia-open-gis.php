7052238774b478605558fd2a181040324663a46d
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2024-06-23 02:27:19 +0900

    updated caps and taxonomies

--- geolonia-open-gis.php
+++ geolonia-open-gis.php
@@ -28,8 +28,131 @@ if ( ! defined( 'GEOLONIA_GIS_DEFAULT_STYLE' ) ) {
 	define( 'GEOLONIA_GIS_DEFAULT_STYLE', 'geolonia/gsi' );
 }
 
+register_activation_hook( __FILE__, function() {
+    $administrator = get_role( 'administrator' );
+
+	$capabilities = array(
+		'publish_posts' 		=> 'publish_maps',
+		'edit_posts' 			=> 'edit_maps',
+		'edit_others_posts' 	=> 'edit_others_maps',
+		'delete_posts' 			=> 'delete_maps',
+		'delete_others_posts' 	=> 'delete_others_maps',
+		'read_private_posts' 	=> 'read_private_maps',
+		'edit_post' 			=> 'edit_map',
+		'delete_post' 			=> 'delete_map',
+		'read_post' 			=> 'read_map',
+		'manage_terms' 			=> 'manage_map_tags',
+		'edit_terms' 			=> 'edit_map_tags',
+		'delete_terms' 			=> 'delete_map_tags',
+		'assign_terms' 			=> 'assign_map_tags',
+	);
+
+    foreach ( $capabilities as $cap ) {
+        $administrator->add_cap( $cap );
+    }
+
+	$editor = get_role( 'editor' );
+
+	$capabilities = array(
+		'publish_posts' 		=> 'publish_maps',
+		'edit_posts' 			=> 'edit_maps',
+		'edit_others_posts' 	=> 'edit_others_maps',
+		'delete_posts' 			=> 'delete_maps',
+		'delete_others_posts' 	=> 'delete_others_maps',
+		'read_private_posts' 	=> 'read_private_maps',
+		'edit_post' 			=> 'edit_map',
+		'delete_post' 			=> 'delete_map',
+		'read_post' 			=> 'read_map',
+		'manage_terms' 			=> 'manage_map_tags',
+		'edit_terms' 			=> 'edit_map_tags',
+		'delete_terms' 			=> 'delete_map_tags',
+		'assign_terms' 			=> 'assign_map_tags',
+	);
+
+    foreach ( $capabilities as $cap ) {
+        $editor->add_cap( $cap );
+    }
+
+	$author = get_role( 'author' );
+
+	$capabilities = array(
+		'publish_posts' 		=> 'publish_maps',
+		'edit_posts' 			=> 'edit_maps',
+		// 'edit_others_posts' 	=> 'edit_others_maps',
+		'delete_posts' 			=> 'delete_maps',
+		// 'delete_others_posts' 	=> 'delete_others_maps',
+		'read_private_posts' 	=> 'read_private_maps',
+		'edit_post' 			=> 'edit_map',
+		'delete_post' 			=> 'delete_map',
+		'read_post' 			=> 'read_map',
+		// 'manage_terms' 			=> 'manage_map_tags',
+		// 'edit_terms' 			=> 'edit_map_tags',
+		// 'delete_terms' 			=> 'delete_map_tags',
+		'assign_terms' 			=> 'assign_map_tags',
+	);
+
+    foreach ( $capabilities as $cap ) {
+        $author->add_cap( $cap );
+    }
+
+	$contributor = get_role( 'contributor' );
+
+	$capabilities = array(
+		// 'publish_posts' 		=> 'publish_maps',
+		'edit_posts' 			=> 'edit_maps',
+		// 'edit_others_posts' 	=> 'edit_others_maps',
+		'delete_posts' 			=> 'delete_maps',
+		// 'delete_others_posts' 	=> 'delete_others_maps',
+		'read_private_posts' 	=> 'read_private_maps',
+		'edit_post' 			=> 'edit_map',
+		'delete_post' 			=> 'delete_map',
+		'read_post' 			=> 'read_map',
+		// 'manage_terms' 			=> 'manage_map_tags',
+		// 'edit_terms' 			=> 'edit_map_tags',
+		// 'delete_terms' 			=> 'delete_map_tags',
+		'assign_terms' 			=> 'assign_map_tags',
+	);
+
+    foreach ( $capabilities as $cap ) {
+        $contributor->add_cap( $cap );
+    }
+
+	$subscriber = get_role( 'subscriber' );
+
+	$capabilities = array(
+		// 'publish_posts' 		=> 'publish_maps',
+		// 'edit_posts' 			=> 'edit_maps',
+		// 'edit_others_posts' 	=> 'edit_others_maps',
+		// 'delete_posts' 			=> 'delete_maps',
+		// 'delete_others_posts' 	=> 'delete_others_maps',
+		// 'read_private_posts' 	=> 'read_private_maps',
+		// 'edit_post' 			=> 'edit_map',
+		// 'delete_post' 			=> 'delete_map',
+		'read_post' 			=> 'read_map',
+	);
+
+    foreach ( $capabilities as $cap ) {
+        $subscriber->add_cap( $cap );
+    }
+
+	flush_rewrite_rules();
+} );
+
 // Registers the custom post type `maps`.
 add_action( 'init', function() {
+
+	$capabilities = array(
+		'publish_posts' 		=> 'publish_maps',
+		'edit_posts' 			=> 'edit_maps',
+		'edit_others_posts' 	=> 'edit_others_maps',
+		'delete_posts' 			=> 'delete_maps',
+		'delete_others_posts' 	=> 'delete_others_maps',
+		'read_private_posts' 	=> 'read_private_maps',
+		'edit_post' 			=> 'edit_map',
+		'delete_post' 			=> 'delete_map',
+		'read_post' 			=> 'read_map',
+	);
+
 	register_post_type( 'maps', array(
 		'label'                 => 'Map',
 		'public'                => true,
@@ -44,8 +167,27 @@ add_action( 'init', function() {
 		'show_in_rest'          => true,
 		'rest_base'             => GEOLONIA_GIS_POST_TYPE,
 		'rest_controller_class' => 'WP_REST_Posts_Controller',
-		'taxonomies' 			=> array( 'category', 'post_tag' ),
+		// 'taxonomies' 			=> array( 'category', 'post_tag' ),
+        'capability_type' 		=> 'map',
+        'capabilities' 			=> $capabilities,
 	) );
+
+	register_taxonomy(
+		'maptags',
+		'maps',
+		array(
+			'label' => 'タグ',
+			'hierarchical' => false,
+			'public' => true,
+			'show_in_rest' => false,
+			'capabilities' => array(
+				'manage_terms' => 'manage_map_tags',
+				'edit_terms' => 'edit_map_tags',
+				'delete_terms' => 'delete_map_tags',
+				'assign_terms' => 'assign_map_tags',
+			)
+		)
+	);
 } );
 
 // Disable Gutenberg on the back end.
